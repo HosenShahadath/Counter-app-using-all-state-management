aed0096ba0f8d9e2435302a88ebdd04b2bbd3342
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-04-26 06:24:32 +0600

    get x

--- lib/Getx/getx_ui_screen.dart
+++ lib/Getx/getx_ui_screen.dart
@@ -1,10 +1,56 @@
+import 'package:counter_app_using_all_state_management/Getx/counter_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class GetxUiScreen extends StatelessWidget {
   const GetxUiScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final counterController = Get.put(CounterController());
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 8,
+        backgroundColor: Colors.blueAccent,
+        title: const Text(
+          'Counter App with Getx',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Counter Value :', style: TextStyle(fontSize: 20)),
+           Obx(
+             () => Text(
+               counterController.count.toString(),
+               style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+           ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    counterController.decrement();
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 20),
+                FloatingActionButton(
+                  onPressed: () {
+                    counterController.increment();
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
